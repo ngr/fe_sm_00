e3b5f9b647d12dba354a34276412f6777fc5f48e
Tolomuco <klc@list.ru>
2015-06-03 22:01:52 +0300

    fix FORM method

--- core/static/core/js/draw/item.js
+++ core/static/core/js/draw/item.js
@@ -1,7 +1,7 @@
 var token = $("#token").val();
 //--------------- List of items stored in this location ------------ 
 drawItem = function(data) {
-//    console.log(data);
+    console.log(data);
     result = "<ul>" + 
 	"<li class='item_id'>" + data.id + "</li>" + 
     "<li class='item_itype'>" + data.itype + "</li>" + 
@@ -16,7 +16,7 @@ drawItem = function(data) {
 //--------------- Form for sending items to another location -------
 function addSendingForm(itemId) {
 	document.getElementById("formSend" + itemId).innerHTML = "<div>" +
-	"<form id=\"sendingItemsForm\" method=\"patch\" action=\"javascript:sendForm(" + itemId + ");\">" +
+	"<form id=\"sendingItemsForm\" method=\"post\" action=\"javascript:sendForm(" + itemId + ");\">" +
 	"<b>Send item:</b><br>" +
 	"Destination:<br> <select id=\"locations\" name=\"location\" class=\"destinationSendingItems\">" + 
 	"</select><br>" +
@@ -28,11 +28,8 @@ function addSendingForm(itemId) {
 }
 //--------------- AJAX request for geting locations ---------------
 function getLocationsList () {
-//    var token = $("#token").val();
-//	var region = $("#location_region").val();
 	var location = $("#location_id").val();
     jQuery.ajax( {
-//        url: 'http://aws00.grischenko.ru:8333/locations/?idle=True', 
         url: 'http://aws00.grischenko.ru:8333/locations/?region=' + $("#location_region").val(), 
         dataType: 'json',
         type: 'GET',
@@ -40,7 +37,6 @@ function getLocationsList () {
             xhr.setRequestHeader( "Authorization", "Bearer " + token );
         },
         success: function( data ) {
-// и ту же location в которой мы находимся в списке нет смысла показывать.		
             $.each( data, function( i, item ) {
 				if (item.id!=location) {
 					$("<option value=\"" + item.id + "\">" + item.name + " (" + item.design + 
@@ -52,7 +48,6 @@ function getLocationsList () {
 };
 //--------------- Sending items by AJAX ----------------------------
 function sendForm(itemId) {
-//    var token = $("#token").val();
 	var loadData = $("#sendingItemsForm").serialize();       
 	jQuery.ajax( {
 		url: 'http://aws00.grischenko.ru:8333/item/' + itemId + '/',
@@ -83,7 +78,7 @@ function sendForm(itemId) {
 		},
 		error: function(data){
 			console.log("Error:" + JSON.parse(data.responseText).slave);
-//			$("#notification").text(JSON.parse(data.responseText).slave).show().focus();  
+			$("#notification").text(JSON.parse(data.responseText).slave).show().focus();  
 		},
 	});
 }
